0310446f64781feea34faa4d63a05b6763a6d7fb
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-06-05 15:23:07 -0300

    edit cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://barber-shops-reviews.vercel.app/'
+    origins 'https://barber-shops-reviews.vercel.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
